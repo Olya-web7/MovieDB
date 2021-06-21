18e1e5029ced9a1ea4ccee8492ed875e42955d6e
“Olha <shyshova24@gmail.com>
2021-06-21 23:09:50 +0300

    updt

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-/*1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
+/*
 2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
@@ -28,7 +23,9 @@ const movieDB = {
 const background = document.querySelector('.promo__bg'),
     adv = document.querySelectorAll('.promo__adv img'),
     genre = document.querySelector('.promo__genre'),
-    list = document.querySelector('.promo__interactive-list');
+    list = document.querySelector('.promo__interactive-list'),
+    btn = document.querySelector('button'),
+    input = document.querySelector('.adding__input');
 
 background.style.backgroundImage = 'url("img/bg.jpg")';
 
@@ -44,3 +41,14 @@ movieDB.movies.forEach((film, i) => {
     <div class="delete"></div>
     </li>`;
 });
+
+btn.addEventListener('click', addFilm);
+
+function addFilm(e) {
+    e.preventDefault;
+    movieDB.movies.push(input.value);
+}
+/*1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;*/
